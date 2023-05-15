c6d6a2c8a30baa2843ed5c583e80454d24f15ef5
preston176 <prestonnyamweya@gmail.com>
2023-05-15 10:28:22 +0300

    testing

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { react } from "react";
+import { ReactDOM } from "react-dom/client";
 import logo from './logo.svg';
 import './App.css';
 
@@ -5,18 +7,17 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+     
+        <img src="./logo.svg" width="40px" />
+        <h1>Fun facts about React</h1>
+        <ul>
+            <li>Was first released in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 100K stars on GitHub</li>
+            <li>Is maintained by Facebook</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>
+  
       </header>
     </div>
   );
