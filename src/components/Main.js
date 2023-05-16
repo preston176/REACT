e795bedd47d51a1ef4294c5756fbd99eab5cdb85
preston176 <prestonnyamweya@gmail.com>
2023-05-16 19:35:16 +0300

    styling done

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,14 @@
 import React from "react"
 
 export default function Main() {
-    return <h1>Main component</h1>
+    return (
+    <><h1 className="main--title">Fun facts about React</h1>
+    <ul className="unordered-list">
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+    </ul>
+    </>
+    )
 }
